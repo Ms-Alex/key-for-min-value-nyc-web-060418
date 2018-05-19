c46135b4cadc254c8853d9a61f9abb7d5da86d0e
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-19 19:26:44 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,4 +6,10 @@ def key_for_min_value(name_hash)
   name_hash.each do | key, num |
     num_array << num
   end
+  min = num_array[0]
+  i = 1
+  while i < num_array.length
+    min = num_array[i] if min > num_array[i]
+    i += 1
+  end    
 end
